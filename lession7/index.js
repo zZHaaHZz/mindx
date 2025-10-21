73cf7e29d6eb095b7157ba74a497bce8f8f83aa6
zZHaaHZz <madoi4040@gmail.com>
2025-10-21 12:45:45 +0700

    commit_lesssion_7

---
+++ lession7/index.js
@@ -0,0 +1,86 @@
+//====================== Cau 1===================
+
+// sum = 0;
+// for (i = 1; i <= 100; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+//====================== Cau 2===================
+
+// arr = "";
+// for(i = 1; i <= 100; i++){
+//     if (i % 3 === 0 && i % 5 === 0)
+//     {
+//         arr = arr + i + ",";
+//     }
+// }
+// console.log(arr)
+
+//====================== Cau 3===================
+
+// input = 5
+// result = 1
+// for (i = 1; i<= input; i++){
+//     result = result * i;
+// }
+// console.log(result)
+
+//====================== Cau 4===================
+
+
+// for(j = 2; j <= 9; j++){
+//     console.log(`Bang cuu chuong ${j}`)
+//     for(i = 1; i <=10; i++){
+//         console.log(`${i} * ${j} = `, j * i)
+//     }
+// }
+
+//====================== Cau 5===================
+
+// inputCau5 = 35
+// chuoiNhiPhan = ""
+
+// while(inputCau5 > 0){
+
+//     const nhiPhan = inputCau5 % 2;
+
+//     if (nhiPhan == 1){
+//         inputCau5 = (inputCau5 - 1) / 2;
+//     }
+//     else{
+//         inputCau5 = inputCau5 / 2;
+//     }
+
+//     chuoiNhiPhan = nhiPhan + chuoiNhiPhan;
+// }
+// console.log(chuoiNhiPhan)
+
+//================= Cau 6 ====================
+
+// n = 5
+// let ketQua = 0
+
+// for (i = 1; i <= n; i++){
+//     ketQua = i + Math.sqrt(ketQua)
+// }
+
+// console.log(Math.sqrt(ketQua))
+
+//=================== Cau 7 ====================
+
+let n = 5
+
+let ketQua = 0
+
+for (i = n; i >= 1; i--){
+    ketQua = i + Math.sqrt(ketQua)
+}
+
+console.log(Math.sqrt(ketQua))
+
+//
+// 0 = 2 + 0
+//b2 
+// 2 = 1 + /_2
+//b3
