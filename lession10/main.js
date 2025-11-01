337a12735ff222dedf890cfc541dfe5162919fc2
zZHaaHZz <madoi4040@gmail.com>
2025-11-01 17:29:50 +0700

    nop_bai_tap_10

--- lession10/main.js
+++ lession10/main.js
@@ -5,7 +5,7 @@ function fxCau1(x) {
     return ketQuacau1;
 }
 
-console.log("Ket qua cau 1: ", fxCau1(5));
+// console.log("Ket qua cau 1: ", fxCau1(5));
 
 
 //================== Cau 2 ===================
@@ -13,7 +13,7 @@ console.log("Ket qua cau 1: ", fxCau1(5));
 function cau2(width, height) {
     return chuVi = (width + height) * 2;
 }
-console.log("ket qua cau 2: ", cau2(10, 15))
+// console.log("ket qua cau 2: ", cau2(10, 15))
 
 //================== Cau 3 ===================
 
@@ -23,7 +23,7 @@ function cau3(r) {
 
 const KQCau3 = cau3(5)
 
-console.log("Ket qua cau 3: ", (KQCau3).toFixed(2));
+// console.log("Ket qua cau 3: ", (KQCau3).toFixed(2));
 
 //================== Cau 4 ===================
 
@@ -39,7 +39,7 @@ function ktSonguyento(n) {
     return true
 }
 
-console.log("Ket qua cau 4: ", ktSonguyento(6))
+// console.log("Ket qua cau 4: ", ktSonguyento(6))
 
 //================== Cau 5 ===================
 
@@ -50,7 +50,7 @@ function tinhTOng(...rest) {
     }
     return sum;
 }
-console.log("Ket qua cau 5: ", tinhTOng(1, 2))
+// console.log("Ket qua cau 5: ", tinhTOng(1, 2))
 
 //================== Cau 6 ===================
 
@@ -68,7 +68,7 @@ function startEnd(start, end) {
     return sumCau6;
 }
 
-console.log("Ket qua cau 6: ",startEnd(3, 1))
+// console.log("Ket qua cau 6: ",startEnd(3, 1))
 
 //================== Cau 7 ===================
 
@@ -86,4 +86,126 @@ const kiemTraChuoiDoiXung = (str) => {
     console.log("Day la chuoi doi xung!")
 }
 
-kiemTraChuoiDoiXung('madam')
+// kiemTraChuoiDoiXung('madam')
+
+//================== Cau 8===================
+
+function kiemTraSoChan (...arrChan){
+    result = true;
+    for(i = 0; i<arrChan.length; i++){
+        if(arrChan[i] % 2 != 0 ){
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+// console.log("Kiem tra arr co phai toan so chan k: ", kiemTraSoChan(2,4,8))
+
+//================== Cau 9===================
+
+function tinhTongSoDuong (...arrDuong){
+    result9 = 0
+    for(i = 0; i<arrDuong.length; i++){
+        if(arrDuong[i] >=0){
+            result9 = result9 + arrDuong[i];
+        }
+    }
+    return result9;
+}
+// console.log("Tong cac so duong: ", tinhTongSoDuong(3,-1,4,-2,5))
+
+//================== Cau 10===================
+function timSLN (...arrSLN){
+    sln = 0;
+    for(i = 0; i < arrSLN.length; i++){
+        if(arrSLN[i] > sln){
+            sln = arrSLN[i]
+        }
+    }
+
+    return sln
+}
+
+// console.log(timSLN(1, 3, 7, 0, -2))
+
+
+//================== Cau 11===================
+
+function sapXep (...arr11){
+    
+    for(i = 0; i< arr11.length; i++){
+        for(j = 0; j < arr11.length; j++){
+            if(arr11[j] > arr11[j+1]){
+                let temp = arr11[j];
+                arr11[j] = arr11[j+1];
+                arr11[j+1] = temp;
+            }
+        }
+    }
+    return arr11
+}
+
+// console.log(sapXep(12, 33, 45, 7, 89))
+
+//================== Cau 12===================
+
+function bai12(k, arr12) {
+    if (k >= arr12.length){
+        k = arr12.length - 1;
+    } 
+    sln = arr12[0]
+    for(i = 0; i <= k; i++){
+        if(arr12[i] > sln){
+            sln = arr12[i]
+        }
+    }
+    return sln;
+}
+// console.log(bai12(3, [2,5,2,7,9,10]))
+
+//================== Cau 13===================
+
+function timSotrunglonnhat(arr){
+    let arrtrung = [];
+
+    for(i = 0; i< arr.length; i++){
+        for(j= i+1; j<arr.length; j++){
+            if(arr[i] === arr[j] && !arrtrung.includes(arr[i])){
+                arrtrung.push(arr[i]);
+            }
+        }
+    }
+    soLonNhat = 0
+    for(i = 0; i<arrtrung.length; i++){
+        if(arrtrung[i] > soLonNhat){
+            soLonNhat = arrtrung[i]
+        }
+    }
+    return soLonNhat;
+}
+
+// console.log(timSotrunglonnhat([3,7,2,3,2,9,3,5,6,1,1,2,7]))
+
+//================== Cau 14===================
+
+function tachChuoi (arr){
+    arrTach = arr.split(" ")
+
+    return arrTach
+}
+
+// console.log(tachChuoi("This is a test"))
+
+function  tinhTong15(obj) {
+    let tong = 0
+    for ( let key in obj){
+        if(typeof obj[key] === "number"){
+            tong += obj[key]
+        }
+    }
+    return tong
+}
+const data = { a: 1, b: 'hello', c: 3, d: 'world' };
+console.log(tinhTong15(data))
